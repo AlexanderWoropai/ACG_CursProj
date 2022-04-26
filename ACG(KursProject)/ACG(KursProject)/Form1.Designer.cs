668f0565078528629de74543e6a8478c7ef54c1f
AlexanderWoropai <71937498+AlexanderWoropai@users.noreply.github.com>
2022-04-26 21:37:47 +0300

    Add created proj

---
+++ ACG(KursProject)/ACG(KursProject)/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace ACG_KursProject_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(910, 498);
+            this.MainPanel.TabIndex = 0;
+            this.MainPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.MainPanel_Paint);
+            this.MainPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainPanel_MouseClick);
+            this.MainPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainPanel_MouseDown);
+            this.MainPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainPanel_MouseMove);
+            this.MainPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MainPanel_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 498);
+            this.Controls.Add(this.MainPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+    }
+}
+
---
+++ ACG(KursProject)/ACG(KursProject)/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ACG_KursProject_
+{
+    public partial class Form1 : Form
+    {
+        int cnt = 0;
+        Point [][] p;
+        bool mdown;
+        String mode;
+        int catch_line_lindex;
+        bool point_focused;
+        int catch_point_lindex;
+        public Form1()
+        {
+            mode = "Рисуем линию";
+            point_focused = false;
+            catch_point_lindex = -1;
+            catch_line_lindex = -1;
+            mdown = false;
+            InitializeComponent();    
+            p = new Point[100][];
+            for (int i = 0; i < 100; i++) 
+            {
+                p[i] = new Point[2];
+            }
+        }
+
+
+        private void MainPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            mdown = false;
+            if (mode == "Рисуем линию")
+            {
+                p[cnt][1].X = e.X;
+                p[cnt][1].Y = e.Y;
+                cnt++;
+            }
+            if (mode == "Изменяем линию")
+            {
+                p[catch_line_lindex][catch_point_lindex].X = e.X;
+                p[catch_line_lindex][catch_point_lindex].Y = e.Y;
+            }
+            MainPanel.Invalidate();
+            mode = "Рисуем линию";
+        }
+
+        private void MainPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            mdown = true;
+            if (mode == "Рисуем линию")
+            {
+                p[cnt][0].X = e.X;
+                p[cnt][0].Y = e.Y;
+            }
+            if (mode == "Изменяем линию") 
+            {
+                p[catch_line_lindex][catch_point_lindex].X = e.X;
+                p[catch_line_lindex][catch_point_lindex].Y = e.Y;
+            }
+        }
+
+        private void MainPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mdown)
+            {
+                if (mode == "Рисуем линию")
+                {
+                    p[cnt][1].X = e.X;
+                    p[cnt][1].Y = e.Y;
+                }
+                if (mode == "Изменяем линию")
+                {
+                    p[catch_line_lindex][catch_point_lindex].X = e.X;
+                    p[catch_line_lindex][catch_point_lindex].Y = e.Y;
+                }
+            }
+            else
+            {
+                mode = "Рисуем линию";
+                point_focused = false;
+                catch_point_lindex = -1;
+                catch_line_lindex = -1;
+                for (int i = 0; i < cnt; i++) 
+                {
+                    if (Math.Abs(p[i][0].X - e.X) < 5 && Math.Abs(p[i][0].Y - e.Y) < 5) 
+                    {
+                        point_focused = true;
+                        catch_point_lindex = 0;
+                        catch_line_lindex = i;
+                        mode = "Изменяем линию";
+                    }
+                    if (Math.Abs(p[i][1].X - e.X) < 5 && Math.Abs(p[i][1].Y - e.Y) < 5)
+                    {
+                        point_focused = true;
+                        catch_point_lindex = 1;
+                        catch_line_lindex = i;
+                        mode = "Изменяем линию";
+                    }
+                }
+            }
+            MainPanel.Invalidate();
+        }
+
+        private void MainPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            /*
+            // Create pen.
+            Pen blackPen = new Pen(Color.Black);
+
+            // Create points for curve.
+            Point start = new Point(100, 100);
+            Point control1 = new Point(200, 10);
+            Point control2 = new Point(350, 50);
+            Point end = new Point(500, 100);
+
+            // Draw arc to screen.
+            g.DrawBezier(blackPen, start, control1, control2, end);//*/
+
+            /*
+            // Create pen.
+            Pen blackPen = new Pen(Color.Black);
+
+            // Create location and size of ellipse.
+            int x = 200;
+            int y = 200;
+            int width = 200;
+            int height = 100;
+
+            // Draw ellipse to screen.
+            e.Graphics.DrawEllipse(blackPen, x, y, width, height);//*/
+
+
+
+            if (point_focused) 
+            {
+                g.DrawRectangle(new Pen(Color.Red), p[catch_line_lindex][catch_point_lindex].X - 5, p[catch_line_lindex][catch_point_lindex].Y - 5, 10, 10);
+            }
+            for (int i = 0; i < cnt; i++) 
+            {
+                g.DrawLine(new Pen(Color.Black), p[i][0].X, p[i][0].Y, p[i][1].X, p[i][1].Y);
+            }
+            if (mdown) 
+            {
+                g.DrawLine(new Pen(Color.Black), p[cnt][0].X, p[cnt][0].Y, p[cnt][1].X, p[cnt][1].Y);
+            }
+        }
+
+        private void MainPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            //MessageBox.Show(String.Format("X : {0} ; Y : {1}", e.Location.X, e.Location.Y));
+        }
+    }
+}
