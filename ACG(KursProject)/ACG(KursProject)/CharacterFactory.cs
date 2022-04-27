5a3c5bb427f15d1949e9df36e3f295e744b5e428
AlexanderWoropai <71937498+AlexanderWoropai@users.noreply.github.com>
2022-04-27 17:09:06 +0300

    Created CharacterFactory

---
+++ ACG(KursProject)/ACG(KursProject)/CharacterFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ACG_KursProject_
+{
+    class CharacterFactory
+    {
+        public Character Create(string type, PointF center) 
+        {
+            if (type == "Line") return new Line(center);
+            return null;
+        }
+    }
+}
